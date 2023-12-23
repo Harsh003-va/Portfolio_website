694b91f92cda0549230bea388bafc7c0352f48ba
Harshavardhan Atigeri <72183253+Harsh003-va@users.noreply.github.com>
2023-12-23 23:09:05 +0530

    up

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'flex' : 'none';
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+  
+//   // Close the menu when a link is clicked (optional)
+//   var menuLinks = document.querySelectorAll('.menu a');
+//   menuLinks.forEach(function(link) {
+//     link.addEventListener('click', function() {
+//       var menu = document.querySelector('.menu');
+//       menu.style.display = 'none';
+//     });
+//   });
+  
